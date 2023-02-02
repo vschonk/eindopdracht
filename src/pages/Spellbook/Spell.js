ee52db974928d7a9bd3b2a792290aac01d183b2e
vschonk <hundred2netherlands@hotmail.com>
2023-02-02 10:43:01 +0100

    update 5 ofzo

--- src/pages/Spellbook/Spell.js
+++ src/pages/Spellbook/Spell.js
@@ -5,9 +5,11 @@ const Spell =(props) => {
     return(
         <div className="spellcontainer">
         <div className="spell scroller">
-            <h3>{props.index+1}</h3>
-            <h4></h4>{props.name}
-            <p>{props.description}</p>
+            <div className="flexspell">
+                <h3 className="circleheader">#{props.index+1}</h3>
+                <h4 className="spellheader">{props.name}</h4>
+            </div>
+            <p className="spelldescription">{props.description}</p>
         </div>
     </div>
     )
